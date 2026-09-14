353e0e705083c8089ee164839fc48a3f151eeb6b
Mehdi Ez-zahri <100123809+MehdiEz-z@users.noreply.github.com>
2026-09-14 14:24:18 +0100

    fix: reset eau module state on sign out

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import { initAuth } from "./shared/auth.js";
 import { initNav, bootActiveModule } from "./shared/router.js";
 import { resetState as resetMaisonState } from "./maison/data.js";
 import { resetState as resetMaladieState } from "./maladie/data.js";
+import { resetState as resetEauState } from "./eau/data.js";
 
 initNav();
 
@@ -9,10 +10,13 @@ initAuth({
   onSignedOut: async () => {
     resetMaisonState();
     resetMaladieState();
+    resetEauState();
     const maison = await import("./maison/boot.js");
     const maladie = await import("./maladie/boot.js");
+    const eau = await import("./eau/boot.js");
     maison.resetModule();
     maladie.resetModule();
+    eau.resetModule();
     document.getElementById("main").innerHTML = "";
     document.getElementById("subtabs").innerHTML = "";
   },
