845148845bf4dfae7ec88c9d3dbaed420e776e00
Mehdi Ez-zahri <100123809+MehdiEz-z@users.noreply.github.com>
2026-09-14 14:32:41 +0100

    fix: update eau-elec module import paths

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import { initAuth } from "./shared/auth.js";
 import { initNav, bootActiveModule } from "./shared/router.js";
 import { resetState as resetMaisonState } from "./maison/data.js";
 import { resetState as resetMaladieState } from "./maladie/data.js";
-import { resetState as resetEauState } from "./eau/data.js";
+import { resetState as resetEauState } from "./eau-elec/data.js";
 
 initNav();
 
@@ -13,10 +13,10 @@ initAuth({
     resetEauState();
     const maison = await import("./maison/boot.js");
     const maladie = await import("./maladie/boot.js");
-    const eau = await import("./eau/boot.js");
+    const eauElec = await import("./eau-elec/boot.js");
     maison.resetModule();
     maladie.resetModule();
-    eau.resetModule();
+    eauElec.resetModule();
     document.getElementById("main").innerHTML = "";
     document.getElementById("subtabs").innerHTML = "";
   },
